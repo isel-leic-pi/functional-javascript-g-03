518de3eb37d8f6c52b9958db83117077da91f65c
João Viegas <A43589@alunos.isel.pt>
2020-10-19 22:03:50 +0100

    alteracao de ficheiros

--- jviegas_43589/functional javascript/program.js
+++ jviegas_43589/functional javascript/program.js
@@ -47,3 +47,15 @@ function checkUsersValid(goodUsers) {
 
   module.exports = checkUsersValid
   */
+
+  /*************************************** */
+
+
+  function countWords(inputWords) {
+      return inputWords.reduce( (accumulator, currentValue) => {
+            accumulator[currentValue] = accumulator[currentValue] + 1 || 1;
+            return accumulator
+      },{});
+  }
+
+  module.exports = countWords
